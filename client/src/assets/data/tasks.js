51e76a61e99ac10ead49334af82f2ae99d222c9f
Austin Nadler <austinnadler706@gmail.com>
2021-07-11 01:22:24 -0500

    Add files via upload

---
+++ client/src/assets/data/tasks.js
@@ -0,0 +1,8 @@
+const tasks = [
+    { id: 1, projectId: 1, name: "Build frontend with React", description: "Build the frontend with react components" },
+    { id: 2, projectId: 1, name: "Figure out server", description: "Look into node/Express, or MAMP" },
+    { id: 3, projectId: 1, name: "Create MySQL database", description: "Create MySQL database" },
+    { id: 4, projectId: 2, name: "Determine training requirements", description: "Determine what training is required" },
+    { id: 5, projectId: 2, name: "Complete all requirements", description: "Complete all training requirements" }
+];
+export default tasks
