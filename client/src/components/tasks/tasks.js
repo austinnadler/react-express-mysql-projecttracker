d45652e23df70c78593b6f1f6e0eb1124d8b324c
Austin Nadler <austinnadler706@gmail.com>
2021-08-12 19:35:23 -0500

    Added buttons to open the project a task belongs to

--- client/src/components/tasks/tasks.js
+++ client/src/components/tasks/tasks.js
@@ -3,6 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Table from 'react-bootstrap/Table';
 import Axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 class Tasks extends React.Component {
     constructor(props) {
@@ -19,13 +22,17 @@ class Tasks extends React.Component {
         });
     }
 
+    goTo(id, e) {
+        if (e.button === 0) this.props.history.push(`/tasks/${id}`);
+    }
+
     render() {
         return (
             <Container>
                 <Col xs={12} className="mt-3 mb-3 text-center">
                     <h1 className="text-success">Tasks</h1>
                     <div>
-                        Select a project from the projects page to edit tasks.
+                        Select the row button to open the project and edit tasks
                     </div>
                 </Col>
                 <Table striped responsive>
@@ -42,7 +49,10 @@ class Tasks extends React.Component {
                             this.state.tasks.map((t) => {
                                 return (
                                     <tr key={t.id}>
-                                        <td>{t.projectName}</td>
+                                        <td>
+                                            <Button onMouseDown={(e) => this.goTo(t.projectId, e)}><FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon></Button>
+                                            &nbsp;&nbsp;{t.projectName}
+                                        </td>
                                         <td>{t.name}</td>
                                         <td>{t.description}</td>
                                     </tr>
