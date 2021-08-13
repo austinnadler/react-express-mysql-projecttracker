0719d133ab09ba23ff61ff93fafbc8b38a615352
Austin Nadler <austinnadler706@gmail.com>
2021-08-12 19:10:09 -0500

    put difference string calculation in a function so all ifs don't have to be checked

--- client/src/components/timestamp/timestamp.js
+++ client/src/components/timestamp/timestamp.js
@@ -11,51 +11,51 @@ function TimeStamp(props) {
         prefix = "Created";
         datetime = new Date(props.created);
     }
+
+    function getString() {
+        let rawDiff = Math.abs(current - datetime); // milliseconds
+        let diffMin = conditionalRound(rawDiff / (1000 * 60), .5); // minutes
+        let diffHr = conditionalRound(rawDiff / (1000 * 60 * 60), .899); // hours
+        let diffD = conditionalRound(rawDiff / (1000 * 60 * 60 * 24), .899); // days
+        let diffMo = conditionalRound(rawDiff / (1000 * 60 * 60 * 24 * 30), .899); // months
+        let diffYr = conditionalRound(rawDiff / (1000 * 60 * 60 * 24 * 365), .899); // months
+        
+        if(diffD >= 1 && diffMo < 1) {
+            return diffD + "d ago";
+        }
     
-    let diff;
-    let rawDiff = Math.abs(current - datetime); // milliseconds
-    let diffMin = conditionalRound(rawDiff / (1000 * 60)); // minutes
-    let diffHr = conditionalRound(rawDiff / (1000 * 60 * 60)); // hours
-    let diffD = conditionalRound(rawDiff / (1000 * 60 * 60 * 24)); // days
-    let diffMo = conditionalRound(rawDiff / (1000 * 60 * 60 * 24 * 30)); // months
-    let diffYr = conditionalRound(rawDiff / (1000 * 60 * 60 * 24 * 365)); // months
+        if(diffHr >= 1 && diffD < 1) {
+            return diffHr + "h ago";
+        }
     
-    if(diffHr >= 1 && diffD < 1) {
-        diff = diffHr + "h ago";
-    }
-
-    if(diffD >= 1 && diffMo < 1) {
-        diff = diffD + "d ago";
-    }
-
-    if(diffMo >= 1 && diffYr < 1) {
-        diff = diffMo + "mo ago";
-    }
-
-    if(diffYr >= 1) {
-        diff = diffYr + "y ago";
-    }
-
-    if(diffMin >= 1 && diffHr < 1) {
-        diff = diffMin + "m ago";
-    }
-
-    if(diffMin < 1) {
-        diff = "just now";
+        if(diffMo >= 1 && diffYr < 1) {
+            return diffMo + "mo ago";
+        }
+    
+        if(diffYr >= 1) {
+            return diffYr + "y ago";
+        }
+    
+        if(diffMin >= 1 && diffHr < 1) {
+            return diffMin + "m ago";
+        }
+    
+        if(diffMin < 1) {
+            return "just now";
+        }
     }
     
-    return <div className="text-muted">{prefix} {diff}</div>
-}
-
-// Round up if radix of n > .899 else round down
-function conditionalRound(n) {
-    var radix = n % 1.0;
-    if( radix > 0.899 && radix < 1.0 ) {
-        return Math.ceil(n);
+    // Round up if radix of n > .899 else round down
+    function conditionalRound(n, bound) {
+        var radix = n % 1.0;
+        if( radix > bound && radix < 1.0 ) {
+            return Math.ceil(n);
+        }
+        else {
+            return Math.floor(n);
+        }
     }
-    else {
-        return Math.floor(n);
-    }
+    return <div className="text-muted">{prefix} {getString()}</div>
 }
 
 export default TimeStamp;
